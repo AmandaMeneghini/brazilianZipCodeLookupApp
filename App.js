a19ba8015e632e734c4132aa613e3758dd8cffb6
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-04-18 21:51:15 -0300

    feat(style): fixs SafeAreaView style and add results area

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App(){
   const [cep, setCep] = useState('');
 
   return(
-    <SafeAreaView style={styles.SafeAreaView}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.area}>
         <Text style={styles.text}>Digite o CEP desejado</Text>
         <TextInput
@@ -29,6 +29,14 @@ export default function App(){
             Limpar
           </Text>
         </TouchableOpacity>
+      </View>
+
+      <View style={styles.resultArea}>
+        <Text style={styles.resultItemText}>CEP: 79000000</Text>
+        <Text style={styles.resultItemText}>Logradouro: Rua Jesus is</Text>
+        <Text style={styles.resultItemText}>Bairro: Centro</Text>
+        <Text style={styles.resultItemText}>Cidade: São Paulo</Text>
+        <Text style={styles.resultItemText}>Estado: SP</Text>
       </View>
     </SafeAreaView>
   );
@@ -71,5 +79,13 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 22,
     color: '#FFF',
+  },
+  resultArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  resultItemText:{
+    fontSize: 22,
   },
 })
